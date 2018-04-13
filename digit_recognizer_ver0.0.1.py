644abe11265d690f0c95955fc845414abca0805e
tasnim007 <tasnim.uiu.cse@gmail.com>
2018-04-13 21:04:00 +0800

    Python file updated1

--- digit_recognizer_ver0.0.1.py
+++ digit_recognizer_ver0.0.1.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
-import matplotlib.image as mpimg
+#import matplotlib.pyplot as plt
+#import matplotlib.image as mpimg
 #import seaborn as sns
 get_ipython().magic('matplotlib inline')
 
